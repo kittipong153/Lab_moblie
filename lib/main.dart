ca5c30b6d63ad7644dd8f7b2a3b0f46af85c3f14
unknown <premjit_k@su.ac.th>
2025-02-22 16:34:16 +0700

    homework 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
+import 'package:mobile_app/product/product_main_page.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile_app/form_widget/form_simple.dart';
-import 'package:mobile_app/form_widget/registration_form.dart';
-import 'package:mobile_app/form_widget/temp_air.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,11 +7,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AQIPage(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 132, 255, 0)),
+        useMaterial3: true,
+      ),
+      home: const ProductMainPage(),
     );
   }
 }
